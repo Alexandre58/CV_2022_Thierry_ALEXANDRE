9a433a8f3b3c5942ee20a9cef76f62e576f956f7
cherokeeal58 <cherokee.al58@outlook.com>
2022-04-08 21:01:03 +0200

    theme sombre

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /***
  * event show image
  */
@@ -8,23 +7,56 @@ const containerAProposDeMoi = document.querySelector(".aProposDemoi_p");
 const btn_event_fermer = document.querySelector(".btn_event_fermer");
 //show photo experiences professionneles
 //dev
-const developpeurWeb = document.querySelector(".developpeurWeb"); 
-const developpeurWebAffichageImage = document.querySelector(".developpeurWebAffichageImage")
+const developpeurWeb = document.querySelector(".developpeurWeb");
+const developpeurWebAffichageImage = document.querySelector(
+  ".developpeurWebAffichageImage"
+);
 //tour grosse pieces
 const tourneur_image_ouvrir = document.querySelector(".tourneur_image_ouvrir");
-const tournageGrossePiecesffichageImage = document.querySelector(".tournageGrossePiecesffichageImage");
+const tournageGrossePiecesffichageImage = document.querySelector(
+  ".tournageGrossePiecesffichageImage"
+);
 //formation tournage
-const tournageFormationAffichageImage = document.querySelector(".tournageFormationAffichageImage");
+const tournageFormationAffichageImage = document.querySelector(
+  ".tournageFormationAffichageImage"
+);
 const formationPoterie = document.querySelector(".formationPoterie");
-const tournageFormationAffichageImage2 = document.querySelector(".tournageFormationAffichageImage2");
+const tournageFormationAffichageImage2 = document.querySelector(
+  ".tournageFormationAffichageImage2"
+);
 
 const formationPoterie2 = document.querySelector(".formationPoterie2");
 //soudure armature du batiment
 const soudure = document.querySelector(".soudure");
-const soudure_afficher_image = document.querySelector(".soudure_afficher_image");
+const soudure_afficher_image = document.querySelector(
+  ".soudure_afficher_image"
+);
+//color theme
+const dark_light_theme = document.querySelector("#dark_light_theme");
+const container_main = document.querySelector(".container_main");
+const container_header = document.querySelector(".container_header");
+const javascrip_Container = document.querySelector(".javascrip_Container");
+const event_container  = document.querySelector(".event_container");
+const container_sections  = document.querySelector(".container_sections");
+const container_footer  = document.querySelector(".container_footer");
+const btn_event  = document.querySelector(".btn_event");
 
 
 
+
+//color them
+dark_light_theme.addEventListener('change', ()=>{
+  console.log("yes");
+    document.body.classList.toggle('container_main_theme');
+    container_header.classList.toggle('container_header_theme');
+    javascrip_Container.classList.toggle('javascrip_Container_theme');
+    event_container.classList.toggle('event_container_theme');
+    container_sections.classList.toggle('container_sections_theme');
+    container_footer.classList.toggle('container_footer_theme');
+    btn_event.classList.toggle('btn_event_theme');
+    btn_event_fermer.classList.toggle('btn_event_fermer_theme');
+   
+})
 //look/add/close a propos de moi
 btnEvent.addEventListener("click", () => {
   containerAProposDeMoi.classList.add("showAProposDemoi_p");
@@ -39,22 +71,27 @@ btn_event_fermer.addEventListener("click", () => {
   btnEvent.style.display = "block";
 });
 //show photo experiences professionneles
-developpeurWeb.addEventListener("click", ()=> {
-  developpeurWebAffichageImage.classList.toggle("developpeurWebAffichageImage_flex");
-})
-tourneur_image_ouvrir.addEventListener("click", ()=> {
-  tournageGrossePiecesffichageImage.classList.toggle("developpeurWebAffichageImage_flex");
-})
-formationPoterie.addEventListener("click", ()=> {
-  tournageFormationAffichageImage.classList.toggle("developpeurWebAffichageImage_flex");
-})
-formationPoterie2.addEventListener("click", (e)=> {
+developpeurWeb.addEventListener("click", () => {
+  developpeurWebAffichageImage.classList.toggle(
+    "developpeurWebAffichageImage_flex"
+  );
+});
+tourneur_image_ouvrir.addEventListener("click", () => {
+  tournageGrossePiecesffichageImage.classList.toggle(
+    "developpeurWebAffichageImage_flex"
+  );
+});
+formationPoterie.addEventListener("click", () => {
+  tournageFormationAffichageImage.classList.toggle(
+    "developpeurWebAffichageImage_flex"
+  );
+});
+formationPoterie2.addEventListener("click", (e) => {
   e.preventDefault();
-  tournageFormationAffichageImage2.classList.toggle("developpeurWebAffichageImage_flex");
-})
-soudure.addEventListener("click", ()=> {
+  tournageFormationAffichageImage2.classList.toggle(
+    "developpeurWebAffichageImage_flex"
+  );
+});
+soudure.addEventListener("click", () => {
   soudure_afficher_image.classList.toggle("developpeurWebAffichageImage_flex");
-})
-
-
-
+});
